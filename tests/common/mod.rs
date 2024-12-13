52b506b1b4020dc85cfc1e4d5b3f7d392dec6bf1
inkbox <enpassPixiv@protonmail.com>
2024-12-13 13:39:42 +0800

    build: CI Test

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,22 +3,20 @@ use std::time::Duration;
 use selenium::{driver::Driver, option::FirefoxBuilder, SError};
 
 pub fn new_driver() -> Driver {
-    let option = FirefoxBuilder::new()
-        .driver(
-            format!(
-                "{}/geckodriver",
-                std::env::current_dir()
-                    .map_err(|f| SError::message(f.to_string()))
-                    .unwrap()
-                    .display()
-            )
-            .as_str(),
+    let mut option = FirefoxBuilder::new().driver(
+        format!(
+            "{}/geckodriver",
+            std::env::current_dir()
+                .map_err(|f| SError::message(f.to_string()))
+                .unwrap()
+                .display()
         )
-        .add_env("DISPLAY", ":1")
-        .add_env("LC_ALL", "zh_CN.UTF-8")
-        .add_env("LANG", "C.UTF-8")
-        // .private()
-        .build();
+        .as_str(),
+    );
+    if std::env::var("HEADLESS").is_ok() {
+        option = option.head_leass();
+    }
+    let option = option.build();
 
     let d = Driver::new(option).unwrap();
     d.get(
