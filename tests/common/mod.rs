94ce3459712e7ce55bd0af1c788e465024cb17f5
inkbox <enpassPixiv@protonmail.com>
2024-12-16 14:10:07 +0800

    build: 调整CI

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -5,8 +5,16 @@ use selenium::{driver::Driver, option::FirefoxBuilder, SError};
 pub fn new_driver() -> Driver {
     let option = if std::env::var("HEADLESS").is_ok() {
         FirefoxBuilder::new()
-            .host("127.0.0.1")
-            .port(4892)
+            .driver(
+                format!(
+                    "{}/geckodriver",
+                    std::env::current_dir()
+                        .map_err(|f| SError::message(f.to_string()))
+                        .unwrap()
+                        .display()
+                )
+                .as_str(),
+            )
             .head_leass()
     } else {
         FirefoxBuilder::new().driver(
