5ff628bf2affc6ee3a15eae1fd4c59c074fa4e2e
inkbox <enpassPixiv@protonmail.com>
2025-07-25 18:08:25 +0800

    feat: 支持代理

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-fn main() -> Result<(), tinyget::Error> {
+fn main() -> Result<(), ureq::Error> {
     println!("rlib");
 
     // 从 github 上下载源码，因为实在找不到源码，只能下载构建完的js
@@ -12,8 +12,17 @@ fn main() -> Result<(), tinyget::Error> {
     let bytes = if let Ok(f) = std::env::var("MIRROR_JS_FILE") {
         std::fs::read_to_string(f)?
     } else {
-        tinyget::get("https://github.com/SeleniumHQ/selenium/raw/cc5ca35d366268db87f1e510c3813114471740db/rb/lib/selenium/webdriver/atoms/isDisplayed.js")
-            .send().expect("download from github fail").as_str().expect("download from github fail").to_string()
+        ureq::get("https://github.com/SeleniumHQ/selenium/raw/cc5ca35d366268db87f1e510c3813114471740db/rb/lib/selenium/webdriver/atoms/isDisplayed.js")
+            .config().proxy(std::env::var("ALL_PROXY")
+                .or_else(|_| std::env::var("all_proxy") )
+                .or_else(|_|std::env::var("HTTPS_PROXy"))
+                .or_else(|_|std::env::var("https_proxy"))
+                .map_err(|_|ureq::Error::InvalidProxyUrl)
+                .and_then( |f|ureq::Proxy::new(f.as_str()))
+                .map_or(None, |f|Some(f))
+            ).build()
+            .call().expect("download from github fail")
+            .body_mut().read_to_string().expect("download from github fail").to_string()
     };
 
     std::fs::write(
